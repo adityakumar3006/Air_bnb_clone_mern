e4c8b0e059b2aff52c5dc3f992999ed48ddd292b
Aditya kumar <aditya.kumar3006@gmail.com>
2024-05-04 12:14:28 +0530

    eight  commit

--- index.js
+++ index.js
@@ -26,13 +26,33 @@ app.use('/uploads', express.static(__dirname + '/uploads'))
 
 // }));
 // Configure CORS
-const corsOptions = {
-    origin: "https://courageous-pie-d800f4.netlify.app",
-    credentials: true // Allow cookies to be sent from the frontend
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: "https://courageous-pie-d800f4.netlify.app",
+//     credentials: true // Allow cookies to be sent from the frontend
+// };
+// app.use(cors(corsOptions));
 // Enable CORS for all routes
 
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://courageous-pie-d800f4.netlify.app",
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+
+    next();
+});
 // Custom CORS middleware to allow specific headers
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
