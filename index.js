5aaa72ba25007a474e616977904fb62788075ebf
Aditya kumar <aditya.kumar3006@gmail.com>
2024-05-04 11:03:43 +0530

    third commit

--- index.js
+++ index.js
@@ -20,10 +20,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'))
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:5173',
+// app.use(cors({
+//     credentials: true,
+//     origin: 'http://localhost:5173',
 
+// }));
+app.use(cors({
+    origin: "https://effulgent-valkyrie-bbaf59.netlify.app"// Replace this with your Netlify frontend URL
 }));
 
 
